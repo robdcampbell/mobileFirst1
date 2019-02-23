d6c327dc613e4dd72b5a81d69942c736823129c8
Rob Campbell <rdeancampbell@gmail.com>
2019-02-22 23:39:43 -0500

    adjusted singlepage layout with nav bar (incomplete)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,48 @@
 // Animate Smooth Scroll
+
+// NAV
+
+// Home
+$('.home__link').on('click',function(){
+  const images = $('.home').position().top;
+
+  $('html, body').animate(
+    {
+    scrollTop: images
+  },900);
+});
+
+// Projects
+$('.projects__link').on('click',function(){
+  const images = $('#images').position().top;
+
+  $('html, body').animate(
+    {
+    scrollTop: images
+  },900);
+});
+
+// About
+$('.about__link').on('click',function(){
+  const images = $('.about').position().top;
+
+  $('html, body').animate(
+    {
+    scrollTop: images
+  },900);
+});
+
+// Contact
+$('.contact__link').on('click',function(){
+  const images = $('.contact').position().top;
+
+  $('html, body').animate(
+    {
+    scrollTop: images
+  },900);
+});
+
+// View Work BTN
 $('#view-work').on('click',function(){
   const images = $('#images').position().top;
 
@@ -7,3 +51,6 @@ $('#view-work').on('click',function(){
     scrollTop: images
   },900);
 });
+
+
+
