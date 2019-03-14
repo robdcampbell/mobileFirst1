7a2b490995883e9390d5c027732b851ebc4480e1
Rob Campbell <rdeancampbell@gmail.com>
2019-03-14 00:11:26 -0400

    Continue styling the landing title text

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-// Animate Smooth Scroll
+// SMOOTH-SCROLL
+
+// github repo:  https://github.com/cferdinandi/smooth-scroll
 
 console.log('Hi, hi, hi, there.: https://www.youtube.com/watch?v=4Xyfd_8QobQ')
 
@@ -6,60 +8,3 @@ const scroll = new SmoothScroll('a[href*="#"]', {
   speed:900,
   easing: 'easeInOutQuad'
 });
-
-
-            // NAV
-
-            // // Home
-            // $('.home__link').on('click',function(){
-            //   const images = $('.home').position().top;
-
-            //   $('html, body').animate(
-            //     {
-            //     scrollTop: images
-            //   },900);
-            // });
-
-            // // Projects
-            // $('.projects__link').on('click',function(){
-            //   const images = $('.container__projects').position().top;
-
-            //   $('html, body').animate(
-            //     {
-            //     scrollTop: images
-            //   },900);
-            // });
-
-            // // About
-            // $('.about__link').on('click',function(){
-            //   const images = $('.container__about').position().top;
-
-            //   $('html, body').animate(
-            //     {
-            //     scrollTop: images
-            //   },900);
-            // });
-
-            // // Contact
-            // $('.contact__link').on('click',function(){
-            //   const images = $('.container__contact').position().top;
-
-            //   $('html, body').animate(
-            //     {
-            //     scrollTop: images
-            //   },900);
-            // });
-
-
-            // // View Work BTN
-            // $('#view-work').on('click',function(){
-            //   const images = $('#images').position().top;
-
-            //   $('html, body').animate(
-            //     {
-            //     scrollTop: images
-            //   },900);
-            // });
-
-
-
