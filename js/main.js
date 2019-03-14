75300a6eef3c466c518947efe2f69c2a9615d2ed
Rob Campbell <rdeancampbell@gmail.com>
2019-03-13 23:51:36 -0400

    Update scrolling with smooth-scroll (removing jQuery)

--- js/main.js
+++ js/main.js
@@ -1,57 +1,65 @@
 // Animate Smooth Scroll
 
-// NAV
+console.log('Hi, hi, hi, there.: https://www.youtube.com/watch?v=4Xyfd_8QobQ')
 
-// Home
-$('.home__link').on('click',function(){
-  const images = $('.home').position().top;
-
-  $('html, body').animate(
-    {
-    scrollTop: images
-  },900);
-});
-
-// Projects
-$('.projects__link').on('click',function(){
-  const images = $('.container__projects').position().top;
-
-  $('html, body').animate(
-    {
-    scrollTop: images
-  },900);
-});
-
-// About
-$('.about__link').on('click',function(){
-  const images = $('.container__about').position().top;
-
-  $('html, body').animate(
-    {
-    scrollTop: images
-  },900);
-});
-
-// Contact
-$('.contact__link').on('click',function(){
-  const images = $('.container__contact').position().top;
-
-  $('html, body').animate(
-    {
-    scrollTop: images
-  },900);
+const scroll = new SmoothScroll('a[href*="#"]', {
+  speed:900,
+  easing: 'easeInOutQuad'
 });
 
 
-// View Work BTN
-$('#view-work').on('click',function(){
-  const images = $('#images').position().top;
+            // NAV
 
-  $('html, body').animate(
-    {
-    scrollTop: images
-  },900);
-});
+            // // Home
+            // $('.home__link').on('click',function(){
+            //   const images = $('.home').position().top;
+
+            //   $('html, body').animate(
+            //     {
+            //     scrollTop: images
+            //   },900);
+            // });
+
+            // // Projects
+            // $('.projects__link').on('click',function(){
+            //   const images = $('.container__projects').position().top;
+
+            //   $('html, body').animate(
+            //     {
+            //     scrollTop: images
+            //   },900);
+            // });
+
+            // // About
+            // $('.about__link').on('click',function(){
+            //   const images = $('.container__about').position().top;
+
+            //   $('html, body').animate(
+            //     {
+            //     scrollTop: images
+            //   },900);
+            // });
+
+            // // Contact
+            // $('.contact__link').on('click',function(){
+            //   const images = $('.container__contact').position().top;
+
+            //   $('html, body').animate(
+            //     {
+            //     scrollTop: images
+            //   },900);
+            // });
+
+
+            // // View Work BTN
+            // $('#view-work').on('click',function(){
+            //   const images = $('#images').position().top;
+
+            //   $('html, body').animate(
+            //     {
+            //     scrollTop: images
+            //   },900);
+            // });
 
 
 
